585d60c1d2fc8b990f8f960689e98cb83b43aefe
Katarzyna Janus <katarzyna.swiniarska@janusgi.com>
2022-07-30 12:24:22 +0200

    first commit

---
+++ src/main/java/pl/kjanus/ConferenceRoomsReservationSystem/ConferenceRoomsReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.kjanus.ConferenceRoomsReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomsReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomsReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kjanus/ConferenceRoomsReservationSystem/ConferenceRoomsReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kjanus.ConferenceRoomsReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomsReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
